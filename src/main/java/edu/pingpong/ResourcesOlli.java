d00ebea950f95abb9c4e02fe79b2851115767427
BertaVR <bverges@cifpfbmoll.eu>
2021-06-14 20:30:05 +0200

    feat: :sparkles: Hechos dos casos test de resource a la desesperada para compensar que los últimos de service no me salen ni pa atrás :(

---
+++ src/main/java/edu/pingpong/ResourcesOlli.java
@@ -0,0 +1,28 @@
+package edu.pingpong;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import edu.pingpong.examen.ServiceOlli;
+
+@Path("/wellcome")
+public class ResourcesOlli {
+
+    @Inject
+    public ServiceOlli service;
+
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    // curl -w "\n" http://localhost:8080/wellcome/ 
+    // -H "Content-Type: application/x-www-form-urlencoded"
+    public String hello() {
+        Response.status(Response.Status.OK).build();
+        return "Wellcome Ollivanders!";
+    }
+    
+}
